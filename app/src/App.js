6c22259c9fc44ed8742561e679568427c8448f27
Sam Malanchuk <malanchuksam@gmail.com>
2020-09-04 15:56:22 -0400

    updated home screen text and buttons

--- app/src/App.js
+++ app/src/App.js
@@ -2,10 +2,13 @@ import React from 'react';
 import './App.css';
 import Main from './components/Main.js';
 import { Route } from 'react-router-dom';
+import { MDBContainer } from "mdbreact";
 
 function App() {
   return (
-    <Route exact path="/" component={Main} />
+    <MDBContainer>
+      <Route exact path="/" component={Main} />
+    </MDBContainer>
   );
 }
 
--- app/src/components/Main.js
+++ app/src/components/Main.js
@@ -1,9 +1,16 @@
 import React from 'react';
+import { MDBBtn, MDBIcon } from "mdbreact";
 
 function Main() {
   return (
       <div>
-          <h1>Main Landing Screen</h1>
+        <h1>Prayer Room</h1>
+        <MDBBtn color="primary">
+            <MDBIcon icon="plus" className="mr-1" /> Add Prayer
+        </MDBBtn>
+        <MDBBtn color="default">
+            <MDBIcon icon="pray" className="mr-1" /> Pray Now
+        </MDBBtn>
       </div>
   );
 }
