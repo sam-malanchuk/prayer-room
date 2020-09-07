e7483d4a839f49df0899ef82e4f1299d632f4403
Sam Malanchuk <malanchuksam@gmail.com>
2020-09-07 16:07:54 -0400

    added more options to the main screen and directions on use

--- app/src/components/Main.js
+++ app/src/components/Main.js
@@ -5,12 +5,23 @@ function Main() {
   return (
       <div>
         <h1>Prayer Room</h1>
-        <MDBBtn color="primary">
-            <MDBIcon icon="plus" className="mr-1" /> Add Prayer
+        <MDBBtn color="secondary">
+            <MDBIcon icon="plus" className="mr-1" /> Add Person
         </MDBBtn>
-        <MDBBtn color="default">
+        <MDBBtn color="info">
             <MDBIcon icon="pray" className="mr-1" /> Pray Now
         </MDBBtn>
+        <MDBBtn color="secondary">
+            <MDBIcon icon="list" className="mr-1" /> View List
+        </MDBBtn>
+        <hr />
+        <h3>How it works:</h3>
+        <ol>
+            <li>Add a new person to pray for</li>
+            <li>Add things to pray about for that person</li>
+            <li>Add relations of that person to another (commonly spouse)</li>
+            <li>Pray for different categories and topics</li>
+        </ol>
       </div>
   );
 }
