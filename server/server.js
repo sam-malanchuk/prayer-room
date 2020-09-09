ef432f08b857596877a94f9d777ae7c8cc347d18
Sam Malanchuk <malanchuksam@gmail.com>
2020-09-08 21:45:32 -0400

    installed helmet and created the server with a base URL get request

---
+++ server/server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const helmet = require('helmet');
+
+// import any routers here
+
+// initiate the server
+const server = express();
+
+// use helmet 
+server.use(helmet());
+// start server
+server.use(express.json());
+
+// use any imported routers here
+
+// basic get request
+server.get('/', (req, res) => {
+    res.status(200).json("API Endpoint for Prayer Room")
+});
+
+// export server
+module.exports = server;
