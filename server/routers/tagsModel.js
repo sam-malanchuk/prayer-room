000dc2b88f1bea67302c1f949c68d3b67853991a
Sam Malanchuk <malanchuksam@gmail.com>
2020-09-28 14:32:00 -0400

    Tags table model functions for listing all the tags and getting tag by id

--- server/routers/tagsModel.js
+++ server/routers/tagsModel.js
@@ -3,5 +3,18 @@ const db = require('../data/db-config.js');
 
 // export model functions
 module.exports = {
-    
+    listTags,
+    getById,
+    add,
+    remove
+}
+
+// return a list of all the tags
+function listTags() {
+    return db('tags');
+}
+
+// return tag name by id
+function getById(id) {
+    return db('tags').where({id}).first();
 }
