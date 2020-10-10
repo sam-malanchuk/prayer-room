422a504f5badb35bbe582953dc8ed205bb919f43
Sam Malanchuk <malanchuksam@gmail.com>
2020-10-10 16:32:00 -0400

    added Group's Router to the server

--- server/server.js
+++ server/server.js
@@ -6,6 +6,7 @@ const UsersRouter = require('./routers/usersRouter.js');
 const PeopleRouter = require('./routers/peopleRouter.js');
 const PrayersRouter = require('./routers/prayersRouter.js');
 const TagsRouter = require('./routers/tagsRouter.js');
+const GroupsRouter = require('./routers/groupsRouter.js');
 
 // initiate the server
 const server = express();
@@ -20,6 +21,7 @@ server.use('/users', UsersRouter);
 server.use('/people', PeopleRouter);
 server.use('/prayers', PrayersRouter);
 server.use('/tags', TagsRouter);
+server.use('/groups', GroupsRouter);
 
 // basic get request
 server.get('/', (req, res) => {
